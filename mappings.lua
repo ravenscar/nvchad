18f1dbedca4e92440820fa7fbd7befd0e3b51ec9
Paul Nilsson <paul.nilsson@smokeball.com>
2023-09-19 14:46:15 +1000

    initial commit of configs

---
+++ mappings.lua
@@ -0,0 +1,97 @@
+local M = {}
+
+M.disabled = {
+  n = {
+      ["<leader>h"] = "",
+      ["<leader>v"] = "",
+      ["<leader>fh"] = "",
+  }
+}
+
+M.abc = {
+  n = {
+    ["<leader>fr"] = {"<cmd> Telescope resume <CR>", "Telescope resume"},
+    ["<C-d>"] = {"<C-d>zz", opts = { remap = false }},
+    ["<C-u>"] = {"<C-u>zz", opts = { remap = false }},
+    ["n"] = {"nzzzv", opts = { remap = false }},
+    ["N"] = {"Nzzzv", opts = { remap = false }},
+    ["<leader>hc"] = {
+      function ()
+        require("nvchad_ui.tabufline").closeAllBufs()
+      end,
+      "Close all buffers"
+    },
+    ["<leader>hx"] = {
+      function ()
+        require("nvchad_ui.tabufline").closeAllBufs()
+        vim.cmd(':q')
+        vim.cmd(':q')
+      end,
+      "Close all buffers"
+    },
+  }
+}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line"
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Run or continue the debugger"
+    }
+  },
+}
+
+M.telescope = {
+  n = {
+    ["<leader>fh"] = { "<CMD>Telescope harpoon marks<CR>", "Find harpooned" },
+    ["<leader>f?"] = { "<CMD>Telescope help_tags<CR>", "Find help" },
+  }
+}
+
+M.harpoon = {
+  n = {
+    ["<leader>ha"] = {
+      function()
+        require("harpoon.mark").add_file()
+      end,
+      "Harpoon file",
+    },
+    ["<leader>hb"] = {
+      function()
+        require("harpoon.ui").toggle_quick_menu()
+      end,
+      "Harpoon menu",
+    },
+    ["<leader>1"] = {
+      function()
+        require("harpoon.ui").nav_file(1)
+      end,
+      "harpoon 1",
+    },
+    ["<leader>2"] = {
+      function()
+        require("harpoon.ui").nav_file(2)
+      end,
+      "harpoon 2",
+    },
+    ["<leader>3"] = {
+      function()
+        require("harpoon.ui").nav_file(3)
+      end,
+      "harpoon 3",
+    },
+    ["<leader>4"] = {
+      function()
+        require("harpoon.ui").nav_file(4)
+      end,
+      "harpoon 4",
+    },
+  },
+}
+
+return M
---
+++ plugins.lua
@@ -0,0 +1,63 @@
+local plugins = {
+  {
+    "ThePrimeagen/harpoon",
+    cmd = "Harpoon"
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      require("dapui").setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      require "custom.configs.dap"
+      require("core.utils").load_mappings("dap")
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function()
+      require "custom.configs.lint"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "js-debug-adapter",
+        "typescript-language-server",
+      },
+    },
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = function()
+      return require "custom.configs.nvimtree"
+    end,
+  }
+}
+return plugins
