faaac15720fd5d1fd1c55bb15c8b302ca6b753ce
Paul Nilsson <paul.nilsson@smokeball.com>
2024-02-24 09:50:49 +1100

    add trouble

--- plugins.lua
+++ plugins.lua
@@ -20,6 +20,16 @@ local plugins = {
     lazy = false
   },
   {
+    "folke/trouble.nvim",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+     -- your configuration comes here
+     -- or leave it empty to use the default settings
+     -- refer to the configuration section below
+    },
+  },
+  {
     "ThePrimeagen/harpoon",
     cmd = "Harpoon"
   },
