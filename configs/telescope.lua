0dba9008eee20b4d72fc576599c7e8500d640a30
Paul Nilsson <paul.nilsson@smokeball.com>
2024-07-18 08:41:42 +1000

    exclude lock files from telescope file search

--- configs/telescope.lua
+++ configs/telescope.lua
@@ -1,7 +1,10 @@
-require('telescope').setup{
-  defaults = {
-    file_ignore_patterns = {
-      "node_modules"
-    }
-  }
+local options = require("plugins.configs.telescope")
+
+options.defaults.file_ignore_patterns = {
+  "node_modules",
+  "package%-lock.json",
+  "pnpm%-lock.yaml",
+  "yarn.lock",
 }
+
+return options
